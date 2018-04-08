03d998c46e6a25516a69f9fe228ed48b666d4179
Jorge Aparicio <jorge@japaric.io>
2018-04-08 22:24:17 +0200

    s/Xargo/Cargo/

--- ci/script.sh
+++ ci/script.sh
@@ -3,9 +3,8 @@ set -ex
 main() {
     local svd=STMicro/STM32F30x.svd
 
+    cargo check --target $TARGET
     if [ $TARGET = x86_64-unknown-linux-gnu ]; then
-        cargo check
-
         # check than the patch can be applied to the original SVD
         local url=https://github.com/posborne/cmsis-svd/raw/python-0.4/data/$svd
         local td=$(mktemp -d)
@@ -16,8 +15,6 @@ main() {
         patch -p1 $svd < $patch
 
         rm -rf $td
-    else
-        xargo check --target $TARGET
     fi
 }
 
